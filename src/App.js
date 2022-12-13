d259d93857608409744af06a96472d1474ec7144
Luis <l-serrato@live.com>
2022-12-13 15:27:11 -0300

    1?

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ Login } />
+          <Route exact path="/" component={ Login } />
           <Route path="/search" component={ Search } />
           <Route path="/album:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" component={ Profile } />
+          <Route exact path="/profile" component={ Profile } />
           <Route path="/profile/edit" component={ ProfileEdit } />
           <Route component={ NotFound } />
         </Switch>
