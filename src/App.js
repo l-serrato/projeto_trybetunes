6d546581330d936c6c187ad77bcf410c96105e1f
Luis <l-serrato@live.com>
2022-12-13 15:52:23 -0300

    1n

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Login } />
-          <Route path="/search" component={ Search } />
-          <Route path="/album:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="*" component={ NotFound } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="*" component={ NotFound } />
         </Switch>
       </BrowserRouter>
     );
